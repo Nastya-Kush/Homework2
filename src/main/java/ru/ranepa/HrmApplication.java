567b549c453d712521a22816d97c63223ffd5c91
Анастасия <nastya.kushnareva2006@gmail.com>
2026-05-05 16:27:21 +0300

    ДЗ2

--- src/main/java/ru/ranepa/HrmApplication.java
+++ src/main/java/ru/ranepa/HrmApplication.java
@@ -2,10 +2,45 @@ package ru.ranepa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import ru.ranepa.model.Employee;
+import ru.ranepa.service.HRMService;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HrmApplication {
     public static void main(String[] args) {
         SpringApplication.run(HrmApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner addTestEmployees(HRMService service) {
+        return args -> {
+            // Проверяем, есть ли уже сотрудники в БД
+            if (service.getAllEmployees().isEmpty()) {
+
+                // Создаем первого сотрудника
+                Employee employee1 = new Employee(
+                        "Кушнарева Анастасия Алексеевна",
+                        "Директор",
+                        BigDecimal.valueOf(300000),
+                        LocalDate.of(2023, 6, 20)
+                );
+                service.addEmployee(employee1);
+                System.out.println("Добавлен сотрудник: " + employee1.getName());
+
+                // Создаем второго сотрудника
+                Employee employee2 = new Employee(
+                        "Гурецкая Дарья Дмитриевна",
+                        "Бизнес аналитик",
+                        BigDecimal.valueOf(120000),
+                        LocalDate.of(2024, 3, 6)
+                );
+                service.addEmployee(employee2);
+                System.out.println("Добавлен сотрудник: " + employee2.getName());
+            }
+        };
     }
 }
